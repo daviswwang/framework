266e5d9b0e2109f00babbf26c24566f266c66664
davis <130086230@qq.com>
2021-01-25 08:22:45 +0800

    up

--- src/Ex/Container/Container.php
+++ src/Ex/Container/Container.php
@@ -22,6 +22,18 @@ class Container implements ArrayAccess, ContainerContract
 
     protected $bindings;
 
+    protected $alias = [];
+
+    protected $resolvingCallbacks;
+
+    protected $globalResolvingCallbacks;
+
+    protected $globalAfterResolvingCallbacks;
+
+    protected $afterResolvingCallbacks;
+
+    protected $resolved;
+
 
     public function get($id)
     {
@@ -87,6 +99,136 @@ class Container implements ArrayAccess, ContainerContract
         //带实例化好的参数对象来 生成并注入新对象
         return $ref->newInstanceArgs($instances);
     }
+
+    public function make($abstract, $arg = [])
+    {
+        return $this->resolve($abstract, $arg);
+    }
+
+    public function getAlias($abstract)
+    {
+        return $this->alias[$abstract] ?? '';
+    }
+
+    //解析容器
+    protected function resolve($abstract, $arg)
+    {
+        //先看看有没有别名
+        $abstract = $this->getAlias($abstract);
+
+        $needsContextualBuild = !empty($arg);
+
+        //有实例化好,在容器内的直接返回
+        if (isset($this->instances[$abstract]) && !$needsContextualBuild) {
+            return $this->instances[$abstract];
+        }
+
+        //尝试获取闭包数据
+        $concrete = $this->getConcrete($abstract);
+
+        //判断闭包内是否已经实例化
+
+        //判断是否为闭包或者是否相等
+        if ($this->isBuildable($concrete, $abstract)) {
+            $object = $this->build($concrete);
+        } else {
+            //递归解析容器
+            $object = $this->make($concrete);
+        }
+
+        //将不需要构造参数的 实例放入 instances
+        if ($this->isShared($abstract) && !$needsContextualBuild) {
+            $this->instances[$abstract] = $object;
+        }
+
+        $this->fireResolvingCallbacks($abstract, $object);
+
+        // Before returning, we will also set the resolved flag to "true" and pop off
+        // the parameter overrides for this build. After those two things are done
+        // we will be ready to return back the fully constructed class instance.
+        $this->resolved[$abstract] = true;
+
+        return $object;
+
+    }
+
+    /**
+     * 启动所有正在解析的回调
+     * @param $abstract
+     * @param $object
+     * @author: fanxinyu
+     */
+    protected function fireResolvingCallbacks($abstract, $object)
+    {
+        $this->fireCallbackArray($object, $this->globalResolvingCallbacks);
+
+        $this->fireCallbackArray(
+            $object, $this->getCallbacksForType($abstract, $object, $this->resolvingCallbacks)
+        );
+
+        $this->fireCallbackArray($object, $this->globalAfterResolvingCallbacks);
+
+        $this->fireCallbackArray(
+            $object, $this->getCallbacksForType($abstract, $object, $this->afterResolvingCallbacks)
+        );
+
+    }
+
+    protected function getCallbacksForType($abstract, $object, array $callbacksPerType)
+    {
+        $results = [];
+
+        foreach ($callbacksPerType as $type => $callbacks) {
+            if ($type === $abstract || $object instanceof $type) {
+                $results = array_merge($results, $callbacks);
+            }
+        }
+
+        return $results;
+    }
+
+
+    protected function fireCallbackArray($object, $callbacks)
+    {
+        foreach ($callbacks as $v) {
+            $v($object, $this);
+        }
+    }
+
+
+    public function isShared($abstract)
+    {
+        return isset($this->instances[$abstract]) ||
+            (isset($this->bindings[$abstract]['shared']) &&
+                $this->bindings[$abstract]['shared'] === true);
+    }
+
+    protected function getExtenders($abstract)
+    {
+        $abstract = $this->getAlias($abstract);
+
+        if (isset($this->extenders[$abstract])) {
+            return $this->extenders[$abstract];
+        }
+
+        return [];
+    }
+
+    protected function isBuildable($concrete, $abstract)
+    {
+        return $concrete === $abstract || $concrete instanceof Closure;
+    }
+
+    protected function getConcrete($abstract)
+    {
+
+        if (isset($this->bindings[$abstract])) {
+            return $this->bindings[$abstract]['concrete'];
+        }
+
+        return $abstract;
+    }
+
 
     protected function resolveDependencies(array $dependencies)
     {
